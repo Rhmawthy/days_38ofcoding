e3b3570908e8b1c33e56920d928b33f4a9199ee8
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-17 21:45:19 +0800

    Add files via upload

---
+++ days38_ latihan.py
@@ -0,0 +1,13 @@
+#program meminta 2 input angka dari user, jika angka pertama lebih kcil dari angka
+#kedua, tukar isi kedua variabel.
+
+angka1 = int (input("angka pertama"))
+angka2 = int (input("angka kedua"))
+
+
+
+#kondisi
+if angka1 < angka2:
+    print ( angka2,angka1)
+else:
+    print (angka1,angka2)
